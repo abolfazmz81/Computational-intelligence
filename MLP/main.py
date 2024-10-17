f419847e53e609a8f6a9e635c9f0a464a611c47e
abolfazmz81 <abolfazmz81@gmail.com>
2024-10-17 18:33:04 +0330

    feat(MLP): Intialize model and layers

--- MLP/main.py
+++ MLP/main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import keras
 from keras.api.datasets import mnist
+from keras import layers,models
 
 # Load the dataset
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -12,3 +13,15 @@ test_images = test_images.reshape((test_images.shape[0], 28 * 28)).astype('float
 # Preprocess the labels (One-hot encoding)
 train_labels = keras.utils.to_categorical(train_labels, 10)
 test_labels = keras.utils.to_categorical(test_labels, 10)
+
+# Initialize the model
+model = models.Sequential()
+
+# Add the first hidden layer
+model.add(layers.Dense(128,activation="relu",input_shape=(784,)))
+
+# Add the second hidden layer
+model.add(layers.Dense(64,activation="relu"))
+
+# Add the output layer
+model.add(layers.Dense(10,activation="softmax"))
