89d66c6e9ad5984b5cfbc385a4bdd0d195d24085
Abolfazl Mohammadi <abolfazmz81@gmail.com>
2024-11-19 20:07:56 +0330

    feat(SOM): Initialize SOM model

--- SOM/main.py
+++ SOM/main.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import load_iris
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from minisom import MiniSom
+import numpy as np
 
 # Change display sizes
 pd.set_option('display.width', 1000)
@@ -41,3 +43,27 @@ iris_df[columns_to_normalize] = scaler.fit_transform(iris_df[columns_to_normaliz
 
 # Display normalized data
 print(iris_df.head(15))
+
+# Prepare the feature data
+X = iris_df.iloc[:, :-1].values  # Features
+
+# Define SOM parameters
+som_dimensions = (10, 10)  # 10x10 grid
+learning_rate = 0.1
+initial_radius = max(som_dimensions) / 2
+num_iterations = 500
+
+# Initialize SOM
+som = MiniSom(x=som_dimensions[0],
+              y=som_dimensions[1],
+              input_len=X.shape[1],
+              sigma=initial_radius,
+              learning_rate=learning_rate)
+
+# Randomly initialize weights
+som.random_weights_init(X)
+
+# Train the SOM
+# Automatically handles updating the weights and Gradual reduction of learning rate and radius
+som.train_random(data=X, num_iteration=num_iterations)
+
