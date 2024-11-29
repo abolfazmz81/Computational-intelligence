24bacf43401feba9807b9e8bbcc21d06e3fbbb9a
Abolfazl Mohammadi <abolfazmz81@gmail.com>
2024-11-29 15:21:54 +0330

    feat(RL): Implement agent Reward function

--- Reinforcement Learning/Agent.py
+++ Reinforcement Learning/Agent.py
@@ -1,4 +1,21 @@
 # This is a sample Python script.
+import random
+import numpy as np
+import csv
+
+# Q-learning Attributes
+ALPHA = 0.1  # Learning rate
+GAMMA = 0.9  # Discount factor
+EPSILON = 0.2  # Exploration rate (epsilon-greedy)(0.4 for more exploration)
+
+# Reward function
+INTERACTION_REWARDS = {
+    'watched': 1,  # Positive feedback
+    'liked': 2,  # Ultra positive feedback
+    'disliked': -1,  # Negative feedback
+    'skipped': 0  # Neutral feedback
+}
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
